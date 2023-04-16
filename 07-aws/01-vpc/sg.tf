6d88fbdd977255926f84f65ad1fcf568bc740dab
Joshua Mathew <umjoshua@gmail.com>
2023-04-16 19:07:12 +0530

    sg

---
+++ 07-aws/01-vpc/sg.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "levelup-sg" {
+  name   = "levelup-ssh"
+  vpc_id = aws_vpc.levelup-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- 07-aws/01-vpc/variables.tf
+++ 07-aws/01-vpc/variables.tf
@@ -3,3 +3,9 @@ variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
   default = "ap-south-1"
 }
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
